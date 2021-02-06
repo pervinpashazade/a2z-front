d8b5ff5536b63970636197f62e4d6485bd4cb2d3
pervinpashazade <ParvinKHp@code.local>
2021-02-07 03:18:20 +0400

    Product Slider section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,33 @@
 $(document).ready(function () {
-    var swiper = new Swiper('.swiper-container', {
+    var homeSlider = new Swiper('.swiper-container.standart', {
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
+    });
+
+    var productSlider = new Swiper('.swiper-container.products', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+            },
+            992: {
+                slidesPerView: 4,
+                spaceBetween: 45,
+            },
+            1200: {
+                slidesPerView: 6,
+                spaceBetween: 50,
+            }
+        }
     });
 });
