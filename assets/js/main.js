92b071bd8be3e8f12da511d8c64d1144af759609
pervinpashazade <ParvinKHp@code.local>
2021-02-08 03:27:34 +0400

    Brands section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,27 +7,25 @@ $(document).ready(function () {
     });
 
     var productSlider = new Swiper('.swiper-container.products', {
+        slidesPerView: 2,
+        spaceBetween: 0,
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-        // breakpoints: {
-        //     320: {
-        //         slidesPerView: 2,
-        //         spaceBetween: 0,
-        //     },
-        //     768: {
-        //         slidesPerView: 3,
-        //         spaceBetween: 0,
-        //     },
-        //     992: {
-        //         slidesPerView: 4,
-        //         spaceBetween: 0,
-        //     },
-        //     1200: {
-        //         slidesPerView: 5,
-        //         spaceBetween: 0,
-        //     }
-        // }
+        breakpoints: {
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 0,
+            },
+            // 992: {
+            //     slidesPerView: 4,
+            //     spaceBetween: 0,
+            // },
+            1200: {
+                slidesPerView: 5,
+                spaceBetween: -10,
+            }
+        }
     });
 });
